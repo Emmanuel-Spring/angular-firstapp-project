28fc7b8dae333ddbc7a0ad5a7524a3a18766769e
Emmanuel-spring <e.nieto@alumnos.santotomas.cl>
2021-10-27 20:31:20 -0300

    Project Config

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,30 @@ import { AppComponent } from './app.component';
 import {HeaderComponent} from "./header/header.component";
 import {FooterComponent} from "./footer/footer.component";
 import { DirectivaComponent } from './directiva/directiva.component';
+import { ClientesComponent } from './clientes/clientes.component';
+import { ClienteService} from "./clientes/cliente.service";
+import  { Routes, RouterModule} from "@angular/router";
+
+
+const routes: Routes = [
+  {path: '', redirectTo: '/clientes', pathMatch: 'full' },
+  {path: 'directiva', component: DirectivaComponent},
+  {path: 'clientes', component: ClientesComponent}
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    DirectivaComponent
+    DirectivaComponent,
+    ClientesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
-  providers: [],
+  providers: [ClienteService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/clientes/clientes.json.ts
@@ -0,0 +1,27 @@
+import { Cliente } from "./cliente";
+
+
+export const CLIENTES: Cliente [] = [
+  {id: 1, nombre: 'Emmanuel', apellido: 'Nieto', email: 'emmanuel.nieto@gmail.com', createAt: '2021-10-27'},
+  {id: 2, nombre: 'Angelica', apellido: 'Balladares', email: 'aballadares@gmail.com', createAt: '2021-10-27'},
+  {id: 3, nombre: 'Sofia', apellido: 'Andana', email: 'sofia.andana@gmail.com', createAt: '2021-10-27'},
+  {id: 4, nombre: 'Mauricio', apellido: 'Muñoz', email: 'mauricio.muñoz@gmail.com', createAt: '2021-10-27'},
+  {id: 5, nombre: 'Nataly', apellido: 'Ascargortta', email: 'nataly.ascargortta@gmail.com', createAt: '2021-10-27'},
+  {id: 6, nombre: 'Magdalena', apellido: 'Villavicencio', email: 'magda.villavicencio@gmail.com', createAt: '2021-10-27'},
+  {id: 7, nombre: 'Federico', apellido: 'Arruabarrena', email: 'federico.arruabarrena@gmail.com', createAt: '2021-10-27'},
+  {id: 8, nombre: 'Martina', apellido: 'Gorigoitia', email: 'martina.gorigoitia@gmail.com', createAt: '2021-10-27'},
+  {id: 9, nombre: 'Nicolle', apellido: 'Nietshe', email: 'nicolle.n@gmail.com', createAt: '2021-10-27'},
+  {id: 10, nombre: 'Fernanda', apellido: 'Romaira', email: 'fernanda.romaira@gmail.com', createAt: '2021-10-27'},
+  {id: 11, nombre: 'Vladimir', apellido: 'Schevchenko', email: 'vladimir.s@gmail.com', createAt: '2021-10-27'},
+  {id: 12, nombre: 'Angelica', apellido: 'Balladares', email: 'aballadares@gmail.com', createAt: '2021-10-27'},
+  {id: 13, nombre: 'Sofia', apellido: 'Andana', email: 'sofia.andana@gmail.com', createAt: '2021-10-27'},
+  {id: 14, nombre: 'Mauricio', apellido: 'Muñoz', email: 'mauricio.muñoz@gmail.com', createAt: '2021-10-27'},
+  {id: 15, nombre: 'Nataly', apellido: 'Ascargortta', email: 'nataly.ascargortta@gmail.com', createAt: '2021-10-27'},
+  {id: 16, nombre: 'Magdalena', apellido: 'Villavicencio', email: 'magda.villavicencio@gmail.com', createAt: '2021-10-27'},
+  {id: 17, nombre: 'Federico', apellido: 'Arruabarrena', email: 'federico.arruabarrena@gmail.com', createAt: '2021-10-27'},
+  {id: 18, nombre: 'Martina', apellido: 'Gorigoitia', email: 'martina.gorigoitia@gmail.com', createAt: '2021-10-27'},
+  {id: 19, nombre: 'Nicolle', apellido: 'Nietshe', email: 'nicolle.n@gmail.com', createAt: '2021-10-27'},
+  {id: 20, nombre: 'Fernanda', apellido: 'Romaira', email: 'fernanda.romaira@gmail.com', createAt: '2021-10-27'},
+  {id: 21, nombre: 'Vladimir', apellido: 'Schevchenko', email: 'vladimir.s@gmail.com', createAt: '2021-10-27'},
+  {id: 22, nombre: 'Emmanuel', apellido: 'Nieto', email: 'emmanuel.nieto@gmail.com', createAt: '2021-10-27'}
+];
